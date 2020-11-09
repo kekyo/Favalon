47bee194bd76152999de78f624262cdc7a7d4f29
Kouji Matsui <k@kekyo.net>
2020-11-09 22:02:54 +0900

    Added calculator choicer.

--- Favalet.CLR.Core/CLRTypeCalculator.cs
+++ Favalet.CLR.Core/CLRTypeCalculator.cs
@@ -18,65 +18,83 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 using Favalet.Expressions;
+using Favalet.Expressions.Algebraic;
 using Favalet.Internal;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Favalet
 {
     public sealed class CLRTypeCalculator :
         TypeCalculator
     {
+        [DebuggerStepThrough]
+        public sealed class CLRTypeCalculatorChoicer :
+            TypeCalculatorChoicer
+        {
+            private CLRTypeCalculatorChoicer()
+            { }
+
+            public override ChoiceResults ChoiceForAnd(
+                ILogicalCalculator calculator,
+                IExpression left, IExpression right)
+            {
+                // Narrowing
+                if (left is ITypeTerm(Type lt) &&
+                    right is ITypeTerm(Type rt))
+                {
+                    var rtl = lt.IsAssignableFrom(rt);
+                    var ltr = rt.IsAssignableFrom(lt);
+
+                    switch (rtl, ltr)
+                    {
+                        case (true, true):
+                            return ChoiceResults.Equal;
+                        case (true, false):
+                            return ChoiceResults.AcceptRight;
+                        case (false, true):
+                            return ChoiceResults.AcceptLeft;
+                    }
+                }
+
+                return base.ChoiceForAnd(calculator, left, right);
+            }
+
+            public override ChoiceResults ChoiceForOr(
+                ILogicalCalculator calculator,
+                IExpression left, IExpression right)
+            {
+                // Widening
+                if (left is ITypeTerm(Type lt) &&
+                    right is ITypeTerm(Type rt))
+                {
+                    var rtl = lt.IsAssignableFrom(rt);
+                    var ltr = rt.IsAssignableFrom(lt);
+
+                    switch (rtl, ltr)
+                    {
+                        case (true, true):
+                            return ChoiceResults.Equal;
+                        case (true, false):
+                            return ChoiceResults.AcceptLeft;
+                        case (false, true):
+                            return ChoiceResults.AcceptRight;
+                    }
+                }
+
+                return base.ChoiceForOr(calculator, left, right);
+            }
+         
+            public new static readonly CLRTypeCalculatorChoicer Instance =
+                new CLRTypeCalculatorChoicer();
+        }
+        
+        public override IChoicer DefaultChoicer =>
+            CLRTypeCalculatorChoicer.Instance;
+
         protected override IComparer<IExpression>? Sorter =>
             OrderedTypeTermComparer.Instance;
-
-        protected override ChoiceResults ChoiceForAnd(
-            IExpression left, IExpression right)
-        {
-            // Narrowing
-            if (left is ITypeTerm(Type lt) &&
-                right is ITypeTerm(Type rt))
-            {
-                var rtl = lt.IsAssignableFrom(rt);
-                var ltr = rt.IsAssignableFrom(lt);
-
-                switch (rtl, ltr)
-                {
-                    case (true, true):
-                        return ChoiceResults.Equal;
-                    case (true, false):
-                        return ChoiceResults.AcceptRight;
-                    case (false, true):
-                        return ChoiceResults.AcceptLeft;
-                }
-            }
-
-            return base.ChoiceForAnd(left, right);
-        }
-
-        protected override ChoiceResults ChoiceForOr(
-            IExpression left, IExpression right)
-        {
-            // Widening
-            if (left is ITypeTerm(Type lt) &&
-                right is ITypeTerm(Type rt))
-            {
-                var rtl = lt.IsAssignableFrom(rt);
-                var ltr = rt.IsAssignableFrom(lt);
-
-                switch (rtl, ltr)
-                {
-                    case (true, true):
-                        return ChoiceResults.Equal;
-                    case (true, false):
-                        return ChoiceResults.AcceptLeft;
-                    case (false, true):
-                        return ChoiceResults.AcceptRight;
-                }
-            }
-
-            return base.ChoiceForOr(left, right);
-        }
 
         public new static readonly CLRTypeCalculator Instance =
             new CLRTypeCalculator();
--- Favalet.Core/TypeCalculator.cs
+++ Favalet.Core/TypeCalculator.cs
@@ -22,12 +22,15 @@ using Favalet.Expressions.Algebraic;
 using Favalet.Expressions;
 using Favalet.Internal;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Favalet
 {
     public interface ITypeCalculator :
         ILogicalCalculator
     {
+        IChoicer DefaultChoicer { get; }
+        
         IEnumerable<IExpression> SortExpressions(
             Func<IExpression, IExpression> selector,
             IEnumerable<IExpression> enumerable);
@@ -36,6 +39,84 @@ namespace Favalet
     public class TypeCalculator :
         LogicalCalculator, ITypeCalculator
     {
+        [DebuggerStepThrough]
+        public class TypeCalculatorChoicer :
+            LogicalCalculatorChoicer
+        {
+            protected TypeCalculatorChoicer()
+            { }
+
+            public override ChoiceResults ChoiceForAnd(
+                ILogicalCalculator calculator,
+                IExpression left, IExpression right)
+            {
+                // Function variance:
+                if (left is IFunctionExpression(IExpression lp, IExpression lr) &&
+                    right is IFunctionExpression(IExpression rp, IExpression rr))
+                {
+                    var parameter = this.ChoiceForAnd(calculator, lp, rp);
+                    var result = this.ChoiceForAnd(calculator, lr, rr);
+
+                    // Contravariance.
+                    switch (parameter, result)
+                    {
+                        case (ChoiceResults.Equal, ChoiceResults.Equal):
+                            return ChoiceResults.Equal;
+
+                        case (ChoiceResults.Equal, ChoiceResults.AcceptLeft):
+                        case (ChoiceResults.AcceptLeft, ChoiceResults.Equal):
+                        case (ChoiceResults.AcceptLeft, ChoiceResults.AcceptLeft):
+                            return ChoiceResults.AcceptLeft;
+                    
+                        case (ChoiceResults.Equal, ChoiceResults.AcceptRight):
+                        case (ChoiceResults.AcceptRight, ChoiceResults.Equal):
+                        case (ChoiceResults.AcceptRight, ChoiceResults.AcceptRight):
+                            return ChoiceResults.AcceptRight;
+                    }
+                }
+
+                return base.ChoiceForAnd(calculator, left, right);
+            }
+
+            public override ChoiceResults ChoiceForOr(
+                ILogicalCalculator calculator,
+                IExpression left, IExpression right)
+            {
+                // Function variance:
+                if (left is IFunctionExpression(IExpression lp, IExpression lr) &&
+                    right is IFunctionExpression(IExpression rp, IExpression rr))
+                {
+                    var parameter = this.ChoiceForOr(calculator, lp, rp);
+                    var result = this.ChoiceForOr(calculator, lr, rr);
+                
+                    // Covariance.
+                    switch (parameter, result)
+                    {
+                        case (ChoiceResults.Equal, ChoiceResults.Equal):
+                            return ChoiceResults.Equal;
+
+                        case (ChoiceResults.Equal, ChoiceResults.AcceptLeft):
+                        case (ChoiceResults.AcceptLeft, ChoiceResults.Equal):
+                        case (ChoiceResults.AcceptLeft, ChoiceResults.AcceptLeft):
+                            return ChoiceResults.AcceptLeft;
+                    
+                        case (ChoiceResults.Equal, ChoiceResults.AcceptRight):
+                        case (ChoiceResults.AcceptRight, ChoiceResults.Equal):
+                        case (ChoiceResults.AcceptRight, ChoiceResults.AcceptRight):
+                            return ChoiceResults.AcceptRight;
+                    }
+                }
+
+                return base.ChoiceForOr(calculator, left, right);
+            }
+         
+            public new static readonly TypeCalculatorChoicer Instance =
+                new TypeCalculatorChoicer();
+        }
+
+        public override IChoicer DefaultChoicer =>
+            TypeCalculatorChoicer.Instance;
+
         protected override IComparer<IExpression>? Sorter =>
             OrderedExpressionComparer.Instance;
 
@@ -43,68 +124,6 @@ namespace Favalet
             Func<IExpression, IExpression> selector,
             IEnumerable<IExpression> enumerable) =>
             base.SortExpressions(selector, enumerable);
-
-        protected override ChoiceResults ChoiceForAnd(
-            IExpression left, IExpression right)
-        {
-            // Function variance:
-            if (left is IFunctionExpression(IExpression lp, IExpression lr) &&
-                right is IFunctionExpression(IExpression rp, IExpression rr))
-            {
-                var parameter = this.ChoiceForAnd(lp, rp);
-                var result = this.ChoiceForAnd(lr, rr);
-
-                // Contravariance.
-                switch (parameter, result)
-                {
-                    case (ChoiceResults.Equal, ChoiceResults.Equal):
-                        return ChoiceResults.Equal;
-
-                    case (ChoiceResults.Equal, ChoiceResults.AcceptLeft):
-                    case (ChoiceResults.AcceptLeft, ChoiceResults.Equal):
-                    case (ChoiceResults.AcceptLeft, ChoiceResults.AcceptLeft):
-                        return ChoiceResults.AcceptLeft;
-                    
-                    case (ChoiceResults.Equal, ChoiceResults.AcceptRight):
-                    case (ChoiceResults.AcceptRight, ChoiceResults.Equal):
-                    case (ChoiceResults.AcceptRight, ChoiceResults.AcceptRight):
-                        return ChoiceResults.AcceptRight;
-                }
-            }
-
-            return base.ChoiceForAnd(left, right);
-        }
-
-        protected override ChoiceResults ChoiceForOr(
-            IExpression left, IExpression right)
-        {
-            // Function variance:
-            if (left is IFunctionExpression(IExpression lp, IExpression lr) &&
-                right is IFunctionExpression(IExpression rp, IExpression rr))
-            {
-                var parameter = this.ChoiceForOr(lp, rp);
-                var result = this.ChoiceForOr(lr, rr);
-                
-                // Covariance.
-                switch (parameter, result)
-                {
-                    case (ChoiceResults.Equal, ChoiceResults.Equal):
-                        return ChoiceResults.Equal;
-
-                    case (ChoiceResults.Equal, ChoiceResults.AcceptLeft):
-                    case (ChoiceResults.AcceptLeft, ChoiceResults.Equal):
-                    case (ChoiceResults.AcceptLeft, ChoiceResults.AcceptLeft):
-                        return ChoiceResults.AcceptLeft;
-                    
-                    case (ChoiceResults.Equal, ChoiceResults.AcceptRight):
-                    case (ChoiceResults.AcceptRight, ChoiceResults.Equal):
-                    case (ChoiceResults.AcceptRight, ChoiceResults.AcceptRight):
-                        return ChoiceResults.AcceptRight;
-                }
-            }
-
-            return base.ChoiceForOr(left, right);
-        }
 
         public new static readonly TypeCalculator Instance =
             new TypeCalculator();
